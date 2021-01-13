317d5191f56aacd51c57068ed5cfa39d19e300e4
YakinRubaiat <sajratul.rubaiat@enosisbd.com>
2021-01-13 23:26:46 +0600

    Show total count, highest count and indivisual count in zilla

--- src/App.js
+++ src/App.js
@@ -1,48 +1,73 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Tabletop from 'tabletop';
+import React, { Component } from "react";
+import "./App.css";
+import Tabletop from "tabletop";
 
 class App extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
-      data: []
-    }
+      data: [],
+    };
   }
 
   componentDidMount() {
     Tabletop.init({
-      key: '1Bh5AV7LwiiWOlK6G-kVDX8YiWJNEyLrrYU6WEYnb_lg',
-      callback: googleData => {
+      key: "15GsX_Ret5jBnGE6I1dPEH3VKIlLC6Fdz84czK51CoyQ",
+      callback: (googleData) => {
         this.setState({
-          data: googleData
-        })
+          data: googleData,
+        });
       },
-      simpleSheet: true
-    })
+      simpleSheet: true,
+    });
   }
 
+  isEmpty = (obj) => {
+    return Object.keys(obj).length === 0;
+  };
+
+  maxCountZilla = (countOfEveryZilla) => {
+    let maxCountZilla;
+
+    if (!this.isEmpty(countOfEveryZilla)) {
+      maxCountZilla = Object.entries(countOfEveryZilla).sort(
+        (prev, next) => prev[1] - next[1]
+      );
+
+      return maxCountZilla.pop()[0];
+    }
+  };
+
+  renderIndividualZilla = (countOfEveryZilla) => {
+    return Object.keys(countOfEveryZilla).map((item, index) => (
+      <li key={index}>
+        {item} : {countOfEveryZilla[item]}
+      </li>
+    ));
+  };
+
   render() {
-    const { data } = this.state
+    const { data } = this.state;
+    const countOfEveryZilla = {};
+
+    if (data.length) {
+      data.forEach((element) => {
+        if (!countOfEveryZilla[element["জেলা"]]) {
+          countOfEveryZilla[element["জেলা"]] = 1;
+        } else {
+          countOfEveryZilla[element["জেলা"]]++;
+        }
+      });
+    }
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">React + Google Sheets Demo</h1>
-        </header>
         <div id="employee-details">
-          {
-            data.map(obj => {
-              return (
-                <div key={obj.employee}>
-                  <p>{obj.employee}</p>
-                  <p>{obj.favDog}</p>
-                  <img alt={obj.favDog} src={obj.img} />
-                </div>
-              )
-            })
-          }
+          <div style={{ fontSize: "30px" }}>
+            Total count: {data.length} <br></br>
+            Highest cases in a zila: {this.maxCountZilla(countOfEveryZilla)}
+          </div>
+          <ul>{this.renderIndividualZilla(countOfEveryZilla)}</ul>
         </div>
       </div>
     );
